4d56e7c5bc8298218843686522f66957959e4809
LiuSecone <liusicong1998@outlook.com>
2019-03-27 12:27:38 +0800

    Add framework

---
+++ client/json_sender.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import requests
+import json
+
+def json_sender(url, dic):
+    files = json.dumps(dic)
+    print(files)
+    r = requests.post(url, files)
+    print(r.text)
+
+if __name__ == "__main__":
+    json_sender('http://localhost:6088/', {'a' : '1', 'b' : '2', 'c' : '3'})
---
+++ server/server.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import argparse
+import json
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def post():
+    dic = json.loads(request.data)
+    log_recorder(dic)
+    return process_dict(dic)
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Parsing args...')
+    parser.add_argument('--host', type=str, default='localhost')
+    parser.add_argument('--port', type=int, default=6088)
+    return parser.parse_args()
+
+def log_recorder(dic):
+    pass
+
+# 处理的dict请求包括空调调节的请求和前台的查询请求
+# 可以使用数据库等结构来存储
+def process_dict(dic):
+    log_recorder(dic)
+    pass
+    return 'Success!'
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    app.run(host=args.host, port=args.port)
