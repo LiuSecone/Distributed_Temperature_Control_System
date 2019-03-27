1eabdc174c10054fc47e9d633090b57d60258a25
LiuSecone <liusicong1998@outlook.com>
2019-03-27 12:30:55 +0800

    Correct codes format

--- client/json_sender.py
+++ client/json_sender.py
@@ -9,5 +9,6 @@ def json_sender(url, dic):
     r = requests.post(url, files)
     print(r.text)
 
+
 if __name__ == "__main__":
     json_sender('http://localhost:6088/', {'a' : '1', 'b' : '2', 'c' : '3'})
